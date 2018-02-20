8abf754eacf884f14b1ec3b75fe397cfd66d33a3
coderaiser <coderaiser@cloudcmd.io>
2018-02-20 17:07:33 +0200

    chore(package) v2.0.0

---
+++ legacy/jonny.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const tryCatch = require('try-catch');
+
+const {
+    parse,
+    stringify
+} = JSON;
+
+module.exports.parse = (...args) => {
+    const [e, data] = tryCatch(parse, ...args);
+
+    return data;
+};
+
+module.exports.stringify = (...args) => {
+    const [e, data] = tryCatch(stringify, ...args);
+
+    return data;
+};
